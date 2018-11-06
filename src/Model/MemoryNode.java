28b391eacde878c2b7ee8540af09d89c9612279f
JohnStranahan <jasballpythons@msn.com>
2018-11-06 10:37:40 -0500

    Start

---
+++ src/Model/MemoryNode.java
@@ -0,0 +1,95 @@
+package Model;
+
+//This class represents each individual memory
+//node that will be represented in the memory
+//stack. Each node has a reference to the previous
+//and next nodes in the stack.
+
+public class MemoryNode
+{
+    private MemoryNode next;
+    private MemoryNode previous;
+    private boolean[] allocatedBits;
+
+    //Constructor for class memoryNode
+    //@param previous: sets previous node
+    //@param next: sets next node.
+    //@param size: size for allocatedBits array
+    public MemoryNode(MemoryNode previous, MemoryNode next, int size)
+    {
+        this.previous = previous;
+        this.next = next;
+        allocatedBits = new boolean[size];
+    }
+
+    //Constructor for setting up node with
+    //no node references.
+    public MemoryNode()
+    {
+        previous = null;
+        next = null;
+        allocatedBits = new boolean[256];
+    }
+
+    //Mutator for previous node.
+    //@param previous: sets previous node.
+    public void setPrevious(MemoryNode previous)
+    {
+        this.previous = previous;
+    }
+
+    //Accessor for previous node.
+    //@return previous: gets the previous node.
+    public MemoryNode getPrevious()
+    {
+        return previous;
+    }
+
+
+    //Mutator for the next  node.
+    //@param next: sets next node.
+    public void setNext(MemoryNode next)
+    {
+        this.next = next;
+    }
+
+    //Accessor for the next  node.
+    //@return next: gets the next node.
+    public MemoryNode getNext()
+    {
+        return next;
+    }
+
+
+    //Sets values in array to true to simulate
+    //Memory allocation.
+    //@param biteSize: the size of the process being allocated.
+    public void allocate(Process newProcess)
+    {
+        int processSize = newProcess.getSize();
+        
+        for(int i = 0; i < processSize; i++){
+            allocatedBits[i] = true;
+        }
+    }
+
+    //This method clears any allocation in the array.
+    public void clearAllocations()
+    {
+        int length = allocatedBits.length;
+        allocatedBits = new boolean[length];
+    }
+
+    //Accessor method for allocatedBits
+    //@return allocatedBits field.
+    public boolean[] getAllocationArray()
+    {
+        return allocatedBits;
+    }
+
+
+
+
+
+
+}
--- src/Model/Process.java
+++ src/Model/Process.java
@@ -1,10 +1,58 @@
 package Model;
 
-public class Process {
+//This class represents a process object that will contain
+//a size and time to live.
 
-    
+public class Process
+{
+    private final String name;
+    private int size;
+    private int ttl;
+
+    //Constructor for class process.
+    //@param name: name of the process
+    //@param size: size of the process
+    //@param ttl: time to live of the process
+    public Process(String name, int size, int ttl)
+    {
+        this.name = name;
+        this.size = size;
+        this.ttl = ttl;
+    }
+
+    //Accessor for name.
+    //@return name
+    public String getName()
+    {
+        return name;
+    }
+
+    //Mutator for size field.
+    //@param size sets the size.
+    public void setSize(int size)
+    {
+        this.size = size;
+    }
+
+    //Accessor for size.
+    //@return size the size of the process.
+    public int getSize()
+    {
+        return size;
+    }
 
 
+    //Mutator for ttl field.
+    //@param ttl sets the ttl.
+    public void setTTL(int ttl)
+    {
+        this.ttl = ttl;
+    }
 
-
+    //Accessor for ttl.
+    //@return ttl the ttl of the process.
+    public int getTTL()
+    {
+        return ttl;
+    }
 }
