7de54b19d35bd76978f25979c088531b7a6e6e0c
Nic <ncklasala@gmail.com>
2018-11-15 11:34:19 -0500

    added memory model abstract class

---
+++ src/Model/MemoryModel.java
@@ -0,0 +1,41 @@
+package Model;
+
+/*
+* non-buddy system abstract memory
+* buddy-system will be a sub class of this
+* buddy sytem will need split and merge methods
+*/
+
+public abstract class MemoryModel {
+    private int numProcesses;
+    private int freeMemory;
+    private MemoryNode head;
+
+    public MemoryModel(){
+        numProcesses = 0;
+        freeMemory = 256;
+        head = null;
+    }
+
+    public boolean isEmpty(){
+        return numProcesses == 0;
+    }
+    public void empty(){
+        head = null;
+        numProcesses = 0;
+        freeMemory = 256;
+    }
+    abstract void addProcess(Process process){
+
+    }
+    abstract void removeProcess(){
+
+    }
+    public boolean enoughSpace(Process process, int availableMemory){
+        return process.getSize() <= availableMemory;
+    }
+    public void notifyObserver(){
+
+    }
+
+}
