5c4bcb457c4ffc080a61ed882d4ea55bf21d087b
Gene Zaleski <zaleskig8@students.rowan.edu>
2018-12-01 17:05:50 -0500

    First draft of process allocation functionality to linked list of memoryNodes.

--- buddyAllocation.java
+++ buddyAllocation.java
@@ -139,7 +139,68 @@ public class buddyAllocation
 
 	}
 
-	public void allocateProcess()
+	//This method allocates a process to a memoryNode
+	//contained in the stack.
+	//@param Process p to be allocated to stack.
+	public void allocateProcess(Process p)
 	{
+		int pSize = p.getSize();
+		
+		//To allocate a process, we need to traverse the linked list of 
+		//memoryNodes and compare the size of each node to the process 
+		//which is attempting to be allocated
+		memoryNode n = this.mNode;
+		memoryNode smallest = n;
+		int difference = 0;
+		boolean found = false;
+
+		while(found == false){
+			while(n != null){
+				
+			        int blockSize = mNode.getAllocationArray().length;
+			        difference = blockSize - pSize;
+
+			        if((difference < pSize) && (difference > 0) && (n.isAllocated() == false)){
+				        n.allocate(pSize);
+				        found = true;
+			        }
+			        else{
+				        n = n.getNext();
+			        }
+			}
+			splitArray(findSmallest());
+		}
+
+	}
+
+	//This method is a helper method for the allocate
+	//function which finds the correct node to split
+	//while searching for a block of memory that will most
+	//appropriately hold the process.
+	//
+	//@return the smallest non-allocated memoryNode in the list.
+	public memoryNode findSmallest()
+	{
+		memoryNode search = this.mNode;
+
+		if((search.getPrevious() == null) && (search.getNext() == null)){
+			return search;
+		}
+
+		memoryNode smallest = search;
+		boolean found = false;
+
+		while((search.getNext() != null) && (found != true)){
+			memoryNode x = search.getNext();
+
+			if((search.getAllocationArray().length > x.getAllocationArray().length) && (x.isAllocated() == false)){
+				smallest = x;
+				found = true;
+			}
+
+			search = x;
+		}
+
+		return smallest;
 	}
 }
