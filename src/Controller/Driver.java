d81affdaa38a614bfe0c2be7d75ad3c295231066
Nic <ncklasala@gmail.com>
2018-12-13 13:11:09 -0500

    added methods to main. moved buddy allocation into model package

--- src/Controller/Driver.java
+++ src/Controller/Driver.java
@@ -13,13 +13,53 @@ public class Driver {
     static BuddyAllocation memory = new BuddyAllocation();
 
     public static void main(String[] args) {
-        Process process = new Process("dog",10,20,1);
+    	boolean stop = false;
+    	do {
+//    		foreach(Process p in memory model){
+//    			if(m.ttl == 0) {
+//    				memory.endProcess(p);
+////    				notify view
+////    				notify model
+//    			}
+//    		}
+//    		if(user add process) {
+//    	
+//    			if(waitingProcess.isEmpty()) {
+//    				Process p = waitingProcess.dequeue();
+//    				memory.allocateProcess(p);
+////    				notify view
+////    				notify model
+//    			}
+//    			else {
+//    				Process p = new process(params from random)
+  				/*
+  				 * if(proces is to big) {
+  				 * add to queue
+  				 *  notify view
+  				 * notify model
+  				 * }
+  				 * else{
+  				 * memory.allocateProcess(p);
+  				 * notify view
+  				 * notify model
+  				 * }
+    	*/
+
+//    			}
+//    		}
+//    		
+//    		foreach(process in memory model){
+//    			p.setTTL(p.getTTL() from gui);
+//    		}
+    	}while(!stop);
+////        Process process = new Process("dog",10,20,1);
+        
 
     }
 
-    public Queue getWaitingProcess() {
-        return waitingProcess;
-    }
+//    public Queue getWaitingProcess() {
+//        return waitingProcess;
+//    }
 
 
 
@@ -27,21 +67,21 @@ public class Driver {
 //        this.waitingProcess = waitingProcess;
 //    }
 
-    public static void addProcess(int pid, String name, int size, int ttl){
-        /*
-        *the fields of the new process will be generated at random in the view
-        *and passed into the controller
-        */
-        Process newProcess = new Process(name, size, ttl, pid);
-        memory.allocateProcess(newProcess);
-    }
+//    public static void addProcess(int pid, String name, int size, int ttl){
+//        /*
+//        *the fields of the new process will be generated at random in the view
+//        *and passed into the controller
+//        */
+//        Process newProcess = new Process(name, size, ttl, pid);
+//        memory.allocateProcess(newProcess);
+//    }
 
-    public static void endProcess(){
-        /*
-        remove process from view and the buddy system
-         */
-
-    }
+//    public static void endProcess(){
+//        /*
+//        remove process from view and the buddy system
+//         */
+//
+//    }
 
 
 }
