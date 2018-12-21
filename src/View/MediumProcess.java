e25809066ed2774030f57746d21a38b1df807719
craigwert <35707330+craigwert@users.noreply.github.com>
2018-12-21 13:43:51 -0500

    Documentation

--- src/View/MediumProcess.java
+++ src/View/MediumProcess.java
@@ -3,6 +3,7 @@ package View;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+//visual representation of a medium process in the memory stack. color coded yellow.
 public class MediumProcess extends ProcessGui {
 
     public MediumProcess(String name, int size, int timeLeft){
