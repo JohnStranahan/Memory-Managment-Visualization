9f29f1da6a5c2538ca5839ca37a34aaaeee71922
craigwert <35707330+craigwert@users.noreply.github.com>
2018-12-21 13:46:30 -0500

    Documentation

--- src/View/SmallProcess.java
+++ src/View/SmallProcess.java
@@ -3,6 +3,7 @@ package View;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+//visual representation of a small process in the memory stack. color coded green.
 public class SmallProcess extends ProcessGui{
 
     public SmallProcess(String name, int size, int timeLeft) {
