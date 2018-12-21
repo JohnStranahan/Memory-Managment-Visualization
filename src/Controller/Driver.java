fc1e9fba2110f87cb5f56dad8e4f54586e6732e9
craigwert <35707330+craigwert@users.noreply.github.com>
2018-12-21 13:35:43 -0500

    Some Driver Documentation

--- src/Controller/Driver.java
+++ src/Controller/Driver.java
@@ -1,10 +1,11 @@
 package Controller;
 
+//Driver for project
 public class Driver {
 
 	public static void main(String[] args) throws Exception {
 		MemoryController controller = new MemoryController();
-		controller.interact();
+		controller.interact(); //creates MemoryController and tells it to build the GUI
 	}
 }
 
