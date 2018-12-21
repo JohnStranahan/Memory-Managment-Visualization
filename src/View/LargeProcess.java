bfd5b3b5c317cc8e40a6411178435b46838e0357
craigwert <35707330+craigwert@users.noreply.github.com>
2018-12-21 13:42:41 -0500

    Documentation

--- src/View/LargeProcess.java
+++ src/View/LargeProcess.java
@@ -2,7 +2,7 @@ package View;
 
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-
+//visual representation of a large process in the stack. color coded red.
 public class LargeProcess extends ProcessGui {
 
     public LargeProcess(String name, int size, int timeLeft){
